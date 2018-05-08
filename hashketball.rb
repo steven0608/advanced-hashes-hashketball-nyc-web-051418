fcc33e6b146e3c786bbb9558ac2c0694ddd34ebb
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 01:48:47 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -66,3 +66,25 @@ def team_names
 end
   names
 end
+
+def player_numbers(team_name)
+
+  game_hash.each do |side,data|
+    data.each do |x,y|
+      if x == team_name
+        
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
