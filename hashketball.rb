84448829d65d29f71c305ddcc0f6f6bc1e24e7ca
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 02:40:08 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :home=>{
       team_name:"Brooklyn Nets",
       colors:["Black", "White"],
-      :players=>{"Alan Anderson" =>{number:0,shoe:16,points:22,rebounds:12,assists:12,steals:1,blocks:1,slam_dunks:1},
+      :players=>{"Alan Anderson" =>{number:0,shoe:16,points:22,rebounds:12,assists:12,steals:3,blocks:1,slam_dunks:1},
       "Reggie Evans" =>{number:30,shoe:14,points:12,rebounds:12,assists:12,steals:12,blocks:12,slam_dunks:7},
       "Brook Lopez" =>{number:11,shoe:17,points:17,rebounds:19,assists:10,steals:3,blocks:1,slam_dunks:15},
       "Mason Plumlee" =>{number:1,shoe:19,points:26,rebounds:12,assists:6,steals:3,blocks:8,slam_dunks:5},
@@ -85,7 +85,6 @@ def player_stats(player)
     game_hash[:home][:players][player]
 
     else
-      game_hash[:away][:players].include? (player)
        game_hash[:away][:players][player]
       end
 end
